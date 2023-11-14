8d7b7cb0a35ae1a689a064017bfb8d2cafd67a6e
ahmadsarg <ahmed.farouk10@hotmail.com>
2023-11-14 15:21:42 +0000

    Java Error handling update

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -7,8 +7,24 @@ class Dec2Hex
     public static int Arg1;
 
     public static void main(String args[])
-    {
-        Arg1 = Integer.parseInt(args[0]);
+    {// Check if an input argument is provided
+        if (args.length == 0) {
+            System.err.println("Error: Please provide a decimal input.");
+            System.exit(1); // Exit the program with an error code
+        }
+
+        // Validate and convert the input argument to integer
+        int num;
+        try {
+            num = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            // Handle non-integer inputs gracefully
+            System.err.println("Error: Input must be an integer.");
+            System.exit(1); // Exit the program with an error code
+            return; // This is not strictly necessary, but it helps to emphasize that the program will exit
+        }
+ 
+       Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
