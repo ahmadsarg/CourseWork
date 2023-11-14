97ca662f9d40d507e14b3a0d2eec77ffd6f3c501
Ahmad Mohamed Farouk <76843269+ahmadsarg@users.noreply.github.com>
2023-11-14 23:03:42 +0000

    Update Dex2Hex.java

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Dec2Hex {
@@ -17,18 +16,7 @@ public class Dec2Hex {
             int arg1 = Integer.parseInt(args[0]);
 
             // Continue with the rest of your code
-            char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-            int rem;
-            int num;
-            num = arg1;
-            String hexadecimal = "";
-            logger.info(String.format("Converting the Decimal Value %d to Hex...", num));
-
-            while (num != 0) {
-                rem = num % 16;
-                hexadecimal = ch[rem] + hexadecimal;
-                num = num / 16;
-            }
+            String hexadecimal = convertDecimalToHex(arg1);
 
             logger.info(String.format("Hexadecimal representation is: %s", hexadecimal));
 
@@ -44,6 +32,22 @@ public class Dec2Hex {
             logger.severe("Error: Input must be an integer.");
             System.exit(1);
         }
+    }
+
+    public static String convertDecimalToHex(int num) {
+        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem;
+        String hexadecimal = "";
+
+        logger.info(String.format("Converting the Decimal Value %d to Hex...", num));
+
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
+        }
+
+        return hexadecimal;
     }
 
     private static void performAdditionalOperation(int value) {
