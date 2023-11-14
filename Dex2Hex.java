f82fbd31d913a9c9f0512f49670b153b824b2008
ahmadsarg <ahmed.farouk10@hotmail.com>
2023-11-14 21:32:25 +0000

    JAVA UPDATE

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
+import java.util.logging.Logger;
 
-class Dec2Hex {
+public class Dec2Hex {
+
+    private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
 
     public static void main(String args[]) {
         // Check for input argument
         if (args.length == 0) {
-            System.err.println("Error: Please provide an input argument.");
+            logger.severe("Error: Please provide an input argument.");
             System.exit(1);
         }
 
         // Parse the input argument as an integer
         try {
-            int Arg1 = Integer.parseInt(args[0]);
+            int arg1 = Integer.parseInt(args[0]);
 
             // Continue with the rest of your code
-            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-            int rem, num;
-            num = Arg1;
+            char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            int rem;
+            int num;
+            num = arg1;
             String hexadecimal = "";
-            System.out.println("Converting the Decimal Value " + num + " to Hex...");
+            logger.info(String.format("Converting the Decimal Value %d to Hex...", num));
 
             while (num != 0) {
                 rem = num % 16;
@@ -26,14 +30,25 @@ class Dec2Hex {
                 num = num / 16;
             }
 
-            System.out.println("Hexadecimal representation is: " + hexadecimal);
+            logger.info(String.format("Hexadecimal representation is: %s", hexadecimal));
+
+            // Conditionally invoke additional methods based on some criteria
+            if (arg1 > 100) {
+                performAdditionalOperation(arg1);
+            }
+
+            logger.info("THANK YOU");
 
         } catch (NumberFormatException e) {
             // Handle non-integer inputs
-            System.err.println("Error: Input must be an integer.");
+            logger.severe("Error: Input must be an integer.");
             System.exit(1);
         }
     }
-}
 
-/**HELLO THIS IS COOMENT**/
+    private static void performAdditionalOperation(int value) {
+        // This method will be invoked conditionally
+        logger.info(String.format("Performing additional operation for value %d", value));
+        // Add your additional operation logic here
+    }
+}
