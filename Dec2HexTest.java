44f8bf39c08ec7d28a1dbac0365b48491fdd536d
Ahmad Mohamed Farouk <76843269+ahmadsarg@users.noreply.github.com>
2023-11-15 00:09:18 +0000

    Update Dec2HexTest.java

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -4,19 +4,19 @@ import static org.junit.Assert.*;
 public class Dec2HexTest {
 
     @Test
-    public void testValidInput() {
+    public static void testValidInput() {
         assertEquals("Conversion of 255 to hex", "FF", Dec2Hex.convertDecimalToHex(255));
         assertEquals("Conversion of 16 to hex", "10", Dec2Hex.convertDecimalToHex(16));
         // Add more test cases as needed
     }
 
     @Test
-    public void testInvalidInput() {
+    public static void testInvalidInput() {
         Dec2Hex.convertDecimalToHex(Integer.parseInt("InvalidInput"));
     }
 
     @Test
-    public void testAdditionalOperation() {
+    public static void testAdditionalOperation() {
         // Assuming your additional operation is based on some condition
         // You may need to modify this test based on your specific additional operation logic
         assertEquals("Additional operation for value 150", "ExpectedResult", Dec2Hex.performAdditionalOperation(150));
